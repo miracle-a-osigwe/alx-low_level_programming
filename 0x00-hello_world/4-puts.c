3620c02d2fa0f443e779298c199e0ea6468218c3
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-02 15:36:45 -0700

    Update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 /**
- * Use PUTS function to print
+ * my_function - Use PUTS function to print
  */
 
 int main(void)
 {
+	/**
+
+	 * print_something - Prints "Programming is like a building etc"
+
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
+
+	/**
+	 * Return: Returns 0 when completed otherwise error
 
 	return (0);
 }
