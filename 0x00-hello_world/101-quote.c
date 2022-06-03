f7728fb86b43c236a0e15adcc169997463cdd186
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-02 20:30:09 -0700

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,15 @@
 #include <sys/syscall.h>
 
 /**
- *main - Print a string without using PUTS and PRINTF
+ * main - Print a string without using PUTS and PRINTF
  *
  *Return: 1 when completed else error
  */
 
 int main(void)
 {
-	const char report[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char report[]="and that piece of art is useful\" - Dora Korpar, 20\
+15-10-19\n";
 
 	syscall(SYS_write, 1, report, sizeof(report));
 
