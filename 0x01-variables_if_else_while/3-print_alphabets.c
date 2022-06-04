bdc9fad96aca527edbb6823a5ebd7dd8c8dc5a3c
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-04 15:23:15 -0700

    Add print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Display the alphabet
+ *
+ * Return: 0 when completed, else error
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
