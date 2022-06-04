9eaaed6e9090cc6ab677fcd5c3b03fe161e7c26e
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-04 13:54:51 -0700

    Update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/** main - Check if the value n is positive or negative
+/* main - Check if the value n is positive or negative
  *
- *Return: 0 when completed
+ * Return: 0 when completed
  */
 
 int main(void)
@@ -21,7 +21,8 @@ int main(void)
 		printf("%d is negative\n", n);
 	else if (n>0)
 		printf("%d is positive\n", n);
-	else {
+	else
+	{
 		printf("%d is zero\n", n);
 	}
 
