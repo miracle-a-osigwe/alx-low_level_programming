a89e3c2d762e325391e6c67d3f8f6abb9c195d4d
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-04 15:46:48 -0700

    Add alphabt without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Display the alphabet
+ *
+ * Return: 0 when completed, else error
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		if (c == 'e' || c == 'q')
+		{continue;}
+		else
+		{
+			putchar(c);
+		}
+	putchar('\n');
+
+	return (0);
+}
