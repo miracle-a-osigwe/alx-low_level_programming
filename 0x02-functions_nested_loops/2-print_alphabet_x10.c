b6d4efb4c1f14b2cf1e18ed35ede58dfc0d4c51e
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-19 16:20:36 -0700

    Add print_alphabet-10x.c and update the main.h file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Calls _putchar
+ * Description - Prints the alphabet in lower case 10x
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int count = 1;
+
+	while (count <= 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z' && count <= 10)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		count++;
+	}
+
+}
