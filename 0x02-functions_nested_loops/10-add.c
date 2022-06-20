547facf764dd0602a69bf7d143f3152530c85679
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-20 15:11:30 -0700

    Add an addition function and update main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ *add - Function call
+ *
+ *@x: first input
+ *@y: second input
+ *@result: output
+ *Description: Add two integers and return the value
+ *
+ *Return: the result
+ */
+
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
