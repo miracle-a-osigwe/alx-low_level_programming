d37a43fcd101567a53605650e420878c76b91ae5
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-20 13:05:21 -0700

    Add 6-abs.c and update the main.h file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ *_abs - Function call
+ *
+ * @a: input to the function
+ * Description: Prints the absolute value of a given number
+ *
+ * Return: the abs value
+ */
+
+int _abs(int a)
+{
+	return (abs(a));
+}
