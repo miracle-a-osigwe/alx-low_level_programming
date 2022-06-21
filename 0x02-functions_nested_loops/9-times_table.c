f0c4444b3b24e67dd31e8221d0a36ddd96a30073
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-21 13:26:25 -0700

    Add 9 times table and update main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+
+/**
+ *
+ * times_table - Function call
+ *
+ * Description: Prints 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		int y = 0;
+
+		while (y <= 9)
+		{
+			int result = x * y;
+
+			if (result/10)
+			{
+				_putchar(result/10 + '0');
+				_putchar(result%10 + '0');
+
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(result + '0');
+
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			}
+
+			y++;
+		}
+		x++;
+
+		_putchar('\n');
+	}
+}
