56bb722bbb5f141760a310550095124e19c5829b
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-06-22 14:20:24 -0700

    Update print to 98 file

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,30 @@
+#include <stdio.h>
 #include "main.h"
+
 
 /**
  * print_to_98 - Function call
  * @n: input integer to the function
  * Description: Prints any given number to 98
- *
  */
 
 void print_to_98(int n)
 {
-	int end = 98;
-
-	if (n > end)
+	if (n < 98)
 	{
-		for (; n > end; n--)
+		while (n < 98)
 		{
-			if ((n / 10) >= 10)
-			{
-				int a = n / 10;
-
-				if (a >= 10)
-			        {
-					_putchar(a / 10 + '0');
-					_putchar(a % 10 + '0');
-				}
-			}
-			else
-			{
-				_putchar(n / 10 + '0');
-			}
-			_putchar(n % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
-
-			if (n == end)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
+			printf("%d, ", n);
+			n++;
 		}
 	}
-	else if (n < end)
+	else if (n > 98)
 	{
-		for (; n < end; n++)
+		while (n > 98)
 		{
-			if (n < 0)
-			{
-				int b = n * -1;
-
-				_putchar('-');
-
-				if (b / 10)
-				{
-					_putchar(b / 10 + '0');
-				}
-				_putchar(b % 10 + '0');
-			}
-
-			else if (n >= 10)
-			{
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n + '0');
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	_putchar(end / 10 + '0');
-	_putchar(end % 10 + '0');
-	_putchar('\n');
+	printf("98\n");
 }
