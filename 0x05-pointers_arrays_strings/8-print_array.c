35e22c3024136c35d69a991b470ae7120c976e0b
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-02 15:02:25 -0700

    Add print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *print_array - Function call
+ *
+ *Description: Prints nth number of a given array
+ *@a : Given array input
+ *@n : Number of array
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	while (x < n)
+	{
+		if (x + 1 == n)
+		{
+			printf("%d \n", a[x]);
+		}
+		else
+		{
+			printf("%d, ", a[x]);
+		}
+		x++;
+	}
+}
