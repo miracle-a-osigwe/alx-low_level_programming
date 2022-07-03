e98a91b417399c5b5b0a43c8c1f573caf9ac5bf5
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-03 10:23:46 -0700

    Add cap_string.c function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include "main.h"
+#include <ctype.h>
+
+
+/**
+ * *cap_string - Function call
+ *Description: Capitalize the words in a strings
+ *@s: input to the function
+ * Return: capitalized strings
+ */
+
+char *cap_string(char *s)
+{
+	int x = 0, res, p, b;
+
+	while (s[x] != '\0')
+	{
+		res = isdigit(s[x - 1]);
+		p = ispunct(s[x - 1]);
+		b = isblank(s[x - 1]);
+
+		if (s[x] >= 97 && s[x] <= 122 && (p != 0 || b != 0))
+		{
+			if (res != 0)
+			{
+				s[x] = s[x];
+			}
+			else if (s[x - 1] == '-')
+			{
+				s[x] = s[x];
+			}
+			else
+			{
+				s[x] = s[x] -32;
+			}
+		}
+		else if (s[x - 1] == '\n')
+		{
+			s[x] = s[x] - 32;
+		}
+		else
+		{
+			s[x] = s[x];
+		}
+
+		x++;
+	}
+	return (s);
+}
