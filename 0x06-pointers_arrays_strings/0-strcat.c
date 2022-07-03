d501b2fc05b45f1ac097f71e1a7e935f00ddbcd8
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-03 05:12:55 -0700

    Add new files

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strcat - Function call
+ * Description: Concatenate two strings
+ *
+ * @dest : First input string
+ * @src : Second input string
+ *
+ *Return : Concatented strings
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *result = strcat(dest, src);
+
+	return (result);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strncat - Function call
+ * Description: Concatenate two strings until nth value
+ *
+ * @dest : First input string
+ * @src : Second input string
+ * @n : Number of values to concatenate
+ *
+ *Return : Concatented strings
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *result = strcat(dest, src, n);
+
+	return (result);
+}
