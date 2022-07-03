a54683394f13ed3b131ca1b66b907ca1c0469c17
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-03 05:37:25 -0700

    Add custom strcmp function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * *_strcmp - Function call
+ *Description: Compare two strings
+ *
+ *@s1: First input string
+ *@s2: Second input string
+ *
+ *Return: less than, greater than or equal to 0 as outcome
+ */
+
+
+char *_strcmp(char *des, char *s2)
+{
+	char *result = strcmp(s1, s2);
+
+	return (result);
+}
