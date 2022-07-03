2123a33e97fd5a457f4cebf042f4964684f9fa6a
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-03 05:51:39 -0700

    Add revese_array function

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *reverse_array - Function call
+ *Description: reverse an array of integers.
+ *@a: string input
+ *@n: size of array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x = 0, temp;
+
+	while (x < n / 2)
+	{
+		temp = a[x];
+		a[x] = a[n - x - 1];
+		a[n - x - 1] = temp;
+		x++;
+	}
+	_putchar('\n');
+}
