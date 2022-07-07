7f31f1c108599792bce86b14589b4ea2cbb5bb9e
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-07 15:52:58 -0700

    Add numbers printer

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - function call
+ *Description: a function that prints numbers.
+ *@separator: string input to the function
+ *@n: first number to print
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nlist;
+	int i, x = 1, y = (int) n;
+
+	va_start(nlist, n);
+	while (x <= y)
+	{
+		i = va_arg(nlist, int);
+
+		if (x < y)
+		{
+			printf("%d%s", i, separator);
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+		x++;
+	}
+}
