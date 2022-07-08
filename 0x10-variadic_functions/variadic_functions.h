be4020d34e6c921d6913e834c951a5785cbaffa0
miracle-a-osigwe <miracleosigwe6@gmail.com>
2022-07-08 05:50:24 -0700

    Update variadic function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef COMMENT
 /**
  *variadic_functions - C function declarations and macro definitions
  */
+
+#endif
+
+#ifndef MAIN
+#define MAIN
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -10,4 +14,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* MAIN */
