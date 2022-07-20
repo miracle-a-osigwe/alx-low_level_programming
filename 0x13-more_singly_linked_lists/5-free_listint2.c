d6d71fd96caf368436ca468ba64431e76f1b13e2
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-20 20:41:23 +0100

    Update 5-free function 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *current = *head;
 
+	if (current == NULL)
+	{
+		return;
+	}
 	while (current != NULL)
 	{
 		temp = current;
 		current = current->next;
 		free(temp);
 	}
-	if (current == NULL)
-	{
-		*head = NULL;
-	}
+	*head = NULL;
 }
