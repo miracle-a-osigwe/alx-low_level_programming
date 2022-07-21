8dd5a6cce533a616de29df6ef52773149dee4d36
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-21 13:49:29 +0100

    Update the free_listint2 file

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *current;
 
-	if (NULL == head)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -23,5 +23,5 @@ void free_listint2(listint_t **head)
 		current = current->next;
 		free(temp);
 	}
-	head = NULL;
+	*head = NULL;
 }
