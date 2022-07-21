5907e384c5e05e74d9d3e3e7be53abc98c594c20
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-22 00:49:33 +0100

    Add clear_bit function

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * clear_bit - Function call
+ * Description: sets the value of a bit to 0 at a given index.
+ * @n: pointer of an unsigned long int.
+ * @index: index of the bit.
+ * Return: return 1 if successful, else -1.
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	m = 1 << index;
+
+	if (*n & m)
+	{
+		*n ^= m;
+	}
+
+	return (1);
+}
