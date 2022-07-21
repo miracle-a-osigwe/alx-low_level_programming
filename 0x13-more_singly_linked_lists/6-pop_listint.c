eba9d3a0b87d9effc8b17d056ee68cb86b8837cd
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-21 09:14:54 +0100

    Add pop_listint.c function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Function call
+ * @head: pointer to use
+ * Description: A function that deletes the head node
+ * Return: the head node's data or 0 where it failed
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n, resp;
+
+	listint_t *temp;
+
+	temp = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+
+	resp = isdigit(n);
+
+	if (resp != 0)
+	{
+		free(temp);
+		return (0);
+	}
+	else
+	{
+		free(temp);
+		return (n);
+	}
+}
