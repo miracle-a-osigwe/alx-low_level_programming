f77f9272e623e670345c2bdb6da4c857c1a075b2
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-22 00:00:18 +0100

    Add print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - Function call
+ * @n: Input
+ * Description: Function that prints the binary representation of a number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+		{
+			print_binary(n >> 1);
+		}
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
