70a1a697df805cc9cc9b6b1ed0f16a31d8681654
miracle-a-osigwe <miracle.osigwe6@gmail.com>
2022-07-21 17:27:25 +0100

    First commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Function call
+ * @b: input pointing to a string of 0 and 1 chars
+ * Description: A function that converts a binary to an unsigned int
+ * Return: the converted number or 0 if it fails
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int len = strlen(b) - 1, i = 0, power, rem, number;
+
+	while (i <= len)
+	{
+		int c = b[i];
+
+		if (c != 48 && c != 49)
+		{
+			return (0);
+		}
+		i++;
+	}
+	number = atoi(b), i = 0;
+
+	while (number != 0)
+	{
+		rem = number % 10;
+		number /= 10;
+		power = (int) (pow(2, i) + 1e-9);
+		decimal += rem * power;
+		++i;
+	}
+	return (decimal);
+}
